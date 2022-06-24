ff08bca9880a985e529a33c137a9e4b3e869f835
Dan Smestad <hockeydan25@gmail.com>
2022-06-24 11:32:00 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace Pizza_party
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.combosize = new System.Windows.Forms.ComboBox();
+            this.chkListVeggies = new System.Windows.Forms.CheckedListBox();
+            this.chkListDrinks = new System.Windows.Forms.CheckedListBox();
+            this.chkListExtras = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkListMeats = new System.Windows.Forms.CheckedListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // combosize
+            // 
+            this.combosize.FormattingEnabled = true;
+            this.combosize.Items.AddRange(new object[] {
+            "Large",
+            "Medium",
+            "Small",
+            "Personal",
+            "Drink",
+            "Extra Only"});
+            this.combosize.Location = new System.Drawing.Point(55, 25);
+            this.combosize.Name = "combosize";
+            this.combosize.Size = new System.Drawing.Size(121, 21);
+            this.combosize.TabIndex = 0;
+            // 
+            // chkListVeggies
+            // 
+            this.chkListVeggies.FormattingEnabled = true;
+            this.chkListVeggies.Items.AddRange(new object[] {
+            "Green Olive",
+            "Hot peppers",
+            "Mild Peppers",
+            "Mushroom",
+            "Viggie Mix"});
+            this.chkListVeggies.Location = new System.Drawing.Point(56, 269);
+            this.chkListVeggies.Name = "chkListVeggies";
+            this.chkListVeggies.Size = new System.Drawing.Size(120, 94);
+            this.chkListVeggies.Sorted = true;
+            this.chkListVeggies.TabIndex = 1;
+            // 
+            // chkListDrinks
+            // 
+            this.chkListDrinks.FormattingEnabled = true;
+            this.chkListDrinks.Items.AddRange(new object[] {
+            "Cherry Pepsi",
+            "Dr,Pepper",
+            "Ginger Ale",
+            "Lemonade",
+            "Rootbeer"});
+            this.chkListDrinks.Location = new System.Drawing.Point(55, 396);
+            this.chkListDrinks.Name = "chkListDrinks";
+            this.chkListDrinks.Size = new System.Drawing.Size(120, 94);
+            this.chkListDrinks.Sorted = true;
+            this.chkListDrinks.TabIndex = 2;
+            // 
+            // chkListExtras
+            // 
+            this.chkListExtras.FormattingEnabled = true;
+            this.chkListExtras.Items.AddRange(new object[] {
+            "10 Wings",
+            "Bread Sticks",
+            "Dessert"});
+            this.chkListExtras.Location = new System.Drawing.Point(56, 538);
+            this.chkListExtras.Name = "chkListExtras";
+            this.chkListExtras.Size = new System.Drawing.Size(120, 94);
+            this.chkListExtras.Sorted = true;
+            this.chkListExtras.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Section:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(52, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Veggie Choices:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Drink Choices:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label4.Location = new System.Drawing.Point(65, 513);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Extras:";
+            // 
+            // chkListMeats
+            // 
+            this.chkListMeats.FormattingEnabled = true;
+            this.chkListMeats.Items.AddRange(new object[] {
+            "Canadian Bacon",
+            "Ground Beef",
+            "Pepperroni",
+            "Sausage"});
+            this.chkListMeats.Location = new System.Drawing.Point(55, 145);
+            this.chkListMeats.Name = "chkListMeats";
+            this.chkListMeats.Size = new System.Drawing.Size(120, 94);
+            this.chkListMeats.Sorted = true;
+            this.chkListMeats.TabIndex = 8;
+            this.chkListMeats.Tag = "chkListMeats.Text";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(53, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Meat Choices:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Toppings:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(225, 145);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 355);
+            this.listBox1.TabIndex = 11;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(492, 145);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 355);
+            this.listBox2.TabIndex = 12;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(225, 558);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 13;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(377, 558);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(56, 647);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(119, 23);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "Add To Cart";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Visible = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(511, 558);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Order(EXIT)";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(676, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 347);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(1041, 682);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chkListMeats);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkListExtras);
+            this.Controls.Add(this.chkListDrinks);
+            this.Controls.Add(this.chkListVeggies);
+            this.Controls.Add(this.combosize);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pizza party Calculation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combosize;
+        private System.Windows.Forms.CheckedListBox chkListVeggies;
+        private System.Windows.Forms.CheckedListBox chkListDrinks;
+        private System.Windows.Forms.CheckedListBox chkListExtras;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox chkListMeats;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizza_party
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double price = 0.0;          //declaring variable and their values, price will change as program runs.
+        double lPrice = 20.00;      //declaring variable and their values, Large pizza.
+        double mPrice = 15.00;       //declaring variable and their values, medium pizza.
+        double sPrice = 12.00;           //declaring variable and their values, samall pizza.
+        double pPrice = 10.00;           //declaring variable and their values, personal size pizza.
+        double dPrice = 2.00;            //declaring variable and their values, price for drink.
+        double toppings = 3.00;          //declaring variable and their values, topping meat, or veggies.
+        double bwdPrice = 5.00;           //declaring variable and their values, bread, wings dessert price.
+
+        private void btnCalculate_Click(object sender, EventArgs e)  //works will form to calculate .
+        {
+            if (IsValidData())                                          //method to check data validation.
+            {
+
+                string size = Convert.ToString(combosize.SelectedItem);  //declaring size combobox user will seelction from drop down list on form
+
+                listBox1.Items.Add(size);                         //declaring size to add items to list box 1. 
+                listBox2.Items.Add("Total:");                     //adding Total for display in listbox2 for cost.  
+                listBox2.Items.Add("------------");               //addd lines for seperation readability to the listbox totals.  
+
+
+                if (combosize.SelectedIndex == 0)           // chceks index 0 is checked, and completes if true, this is Large. 
+                {
+                    listBox1.Items.Add(lPrice.ToString("c"));   //optput to listbox displaying the price
+                    price = price + lPrice;                      //price for large size is added to price if selected.                                     
+                }
+
+                else if (combosize.SelectedIndex == 1)  // chceks index 0 is checked, and completes if true, this is Large. 
+                {
+                    price = price + mPrice;                      //price for medium size is added to price if selected.
+                    listBox1.Items.Add(mPrice.ToString("c")); //optput to listbox displaying the price.
+                }
+                else if (combosize.SelectedIndex == 2)  // chceks index 0 is checked, and completes if true, this is Large. 
+                {
+                    price = price + sPrice;                     //price for small size is added to price if selected. 
+                    listBox1.Items.Add(sPrice.ToString("c")); //optput to listbox displaying the price
+
+                }
+                else if (combosize.SelectedIndex == 3)  // chceks index 0 is checked, and completes if true, this is Large. 
+                {
+                    price = price + pPrice;                 //price for personal size is added to price if selected. 
+                    listBox1.Items.Add(pPrice.ToString("c")); //optput to listbox displaying the price
+                }
+                listBox1.Items.Add("------------");   //addd lines for seperation readability to the listbox. 
+
+
+                foreach (string meat in chkListMeats.CheckedItems)
+                {
+                    price = price + toppings;                    //price for addional selected itmes is added to price.
+                    listBox1.Items.Add(meat);                       //displays meats selected to listbox.
+                    listBox1.Items.Add(toppings.ToString("c"));   //output to listbox displaying,price total and adding topping cost.  
+                    listBox1.Items.Add("------------");         //addd lines for seperation readability to the listbox totals. 
+                }
+
+                foreach (string veggie in chkListVeggies.CheckedItems)
+                {
+                    price = price + toppings;                    //price for addional selected itmes are added to price. 
+                    listBox1.Items.Add(veggie);                 //displays veggie selected to listbox.
+                    listBox1.Items.Add(toppings.ToString("c"));  //output to listbox displaying,price total and adding topping cost.
+                    listBox1.Items.Add("------------");  //addd lines for seperation readability to the listbox. 
+                }
+
+                foreach (string drink in chkListDrinks.CheckedItems)
+                {
+                    price = price + dPrice;                      //price for addional selected itmes are added to price.
+                    listBox1.Items.Add(drink);                  //displays drinks selected to listbox.
+                    listBox1.Items.Add(dPrice.ToString("c"));  //output to listbox displaying,price total and adding drink cost.
+                    listBox1.Items.Add("------------");   //addd lines for seperation readability to the listbox. 
+                }
+
+                foreach (string extras in chkListExtras.CheckedItems)
+                {
+                    price = price + bwdPrice;                //price for addional selected itmes are added to price. 
+                    listBox1.Items.Add(extras);                 //displays extras selected to listbox.
+                    listBox1.Items.Add(bwdPrice.ToString("c"));  //output to listbox displaying,price total and adding extras cost.
+                    listBox1.Items.Add("------------");   //addd lines for seperation readability to the listbox. 
+                }
+
+                string tprice = price.ToString("c");       //output to listbox displaying the Total price     
+                listBox2.Items.Add(tprice);                //varible delared holding the total price from all check items.        
+            }
+        }
+        private bool IsValidData()         //code for data validation. We are checking box is checked
+        {
+            if (combosize.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a pizza size.",
+                                "Entry Error");
+                combosize.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();              //clearing items from the list of selected pizza items   
+            listBox2.Items.Clear();             // clearing the cost totals
+            combosize.SelectedIndex = -1;       //clearing out the combobox
+            price = 0.0;                        //clearing the price value so we can recalculate the cost.
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // accidental click not needed/or used
+        private void pictureBox1_Click(object sender, EventArgs e)//accidental click not needed/or used
+        {
+        }
+        // accidental click not needed/or used
+        //not used 
+        private void btnAdd_Click(object sender, EventArgs e) //not used  
+        {
+            //not used 
+        }
+    }
+}
